b9bd70b678ef535494956408911a56c767e9bb4c
thZero.com <development@thzero.com>
2019-12-22 10:06:21 -0600

    added support for moment and timestamps

--- src/components/DatetimePicker.vue
+++ src/components/DatetimePicker.vue
@@ -6,7 +6,7 @@
         :disabled="disabled"
         :loading="loading"
         :label="label"
-        :value="formattedDatetime"
+        :value="formattedDateTime"
         v-on="on"
         readonly
       >
@@ -48,7 +48,8 @@
       <v-card-actions>
         <v-spacer></v-spacer>
         <slot name="actions" :parent="this">
-          <v-btn color="grey lighten-1" text @click.native="clearHandler">{{ clearText }}</v-btn>
+          <v-btn color="primary lighten-1" text @click.native="clearHandler">{{ clearText }}</v-btn>
+          <v-btn color="primary lighten-1" text @click.native="cancelHandler">{{ cancelText }}</v-btn>
           <v-btn color="green darken-1" text @click="okHandler">{{ okText }}</v-btn>
         </slot>
       </v-card-actions>
@@ -57,123 +58,123 @@
 </template>
 
 <script>
-import { format, parse } from 'date-fns'
+import moment from 'moment'
+// import { format, parse } from 'date-fns'
 
-const DEFAULT_DATE = ''
-const DEFAULT_TIME = '00:00:00'
-const DEFAULT_DATE_FORMAT = 'yyyy-MM-dd'
-const DEFAULT_TIME_FORMAT = 'HH:mm:ss'
-const DEFAULT_DIALOG_WIDTH = 340
+const DEFAULT_CANCEL_TEXT = 'CANCEL'
 const DEFAULT_CLEAR_TEXT = 'CLEAR'
+const DEFAULT_DATE = ''
+const DEFAULT_DATE_FORMAT = 'YYYY-MM-DD'
+// const DEFAULT_DATE_FORMAT = 'yyyy-MM-dd'
+const DEFAULT_DIALOG_WIDTH = 340
 const DEFAULT_OK_TEXT = 'OK'
+const DEFAULT_TIME = '00:00:00'
+const DEFAULT_TIME_FORMAT = 'HH:mm:ss'
+// const DEFAULT_TIME_FORMAT = 'HH:mm:ss'
+const DEFAULT_TIME_OUTPUT_FORMAT = 'HH:mm'
+const OUTPUT_TYPE_DATE = 'date'
+const OUTPUT_TYPE_TIMESTAMP = 'timestamp'
 
 export default {
   name: 'v-datetime-picker',
-  model: {
-    prop: 'datetime',
-    event: 'input'
-  },
+  // model: {
+  //   prop: 'datetime',
+  //   event: 'input'
+  // },
   props: {
-    datetime: {
-      type: [Date, String],
+    value: {
+      type: [Date, String, Number],
       default: null
     },
-    disabled: {
-      type: Boolean
-    },
-    loading: {
-      type: Boolean
-    },
-    label: {
+    cancelText: {
       type: String,
-      default: ''
-    },
-    dialogWidth: {
-      type: Number,
-      default: DEFAULT_DIALOG_WIDTH
-    },
-    dateFormat: {
-      type: String,
-      default: DEFAULT_DATE_FORMAT
-    },
-    timeFormat: {
-      type: String,
-      default: 'HH:mm'
+      default: DEFAULT_CANCEL_TEXT
     },
     clearText: {
       type: String,
       default: DEFAULT_CLEAR_TEXT
     },
+    dateFormat: {
+      type: String,
+      default: DEFAULT_DATE_FORMAT
+    },
+    datePickerProps: {
+      type: Object
+    },
+    disabled: {
+      type: Boolean
+    },
+    dialogWidth: {
+      type: Number,
+      default: DEFAULT_DIALOG_WIDTH
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    loading: {
+      type: Boolean
+    },
     okText: {
       type: String,
       default: DEFAULT_OK_TEXT
     },
+    outputType: {
+      type: String,
+      default: OUTPUT_TYPE_DATE,
+      validator: (val) => [OUTPUT_TYPE_DATE, OUTPUT_TYPE_TIMESTAMP].includes(val)
+    },
     textFieldProps: {
       type: Object
     },
-    datePickerProps: {
-      type: Object
+    timeFormat: {
+      type: String,
+      default: DEFAULT_TIME_OUTPUT_FORMAT
     },
     timePickerProps: {
       type: Object
     }
   },
-  data() {
-    return {
-      display: false,
-      activeTab: 0,
-      date: DEFAULT_DATE,
-      time: DEFAULT_TIME
-    }
-  },
+  data: () => ({
+    display: false,
+    activeTab: 0,
+    date: DEFAULT_DATE,
+    internalOutputType: OUTPUT_TYPE_DATE,
+    time: DEFAULT_TIME
+  }),
   mounted() {
     this.init()
   },
   computed: {
+    dateSelected() {
+      return !this.date
+    },
     dateTimeFormat() {
       return this.dateFormat + ' ' + this.timeFormat
     },
     defaultDateTimeFormat() {
       return DEFAULT_DATE_FORMAT + ' ' + DEFAULT_TIME_FORMAT
     },
-    formattedDatetime() {
-      return this.selectedDatetime ? format(this.selectedDatetime, this.dateTimeFormat) : ''
+    formattedDateTime() {
+      return this.formatDateTime(this.selectedDatetime)
     },
     selectedDatetime() {
       if (this.date && this.time) {
         let datetimeString = this.date + ' ' + this.time
-        if (this.time.length === 5) {
+        if (this.time.length === 5)
           datetimeString += ':00'
-        }
-        return parse(datetimeString, this.defaultDateTimeFormat, new Date())
-      } else {
-        return null
+
+        let date = moment(datetimeString, this.defaultDateTimeFormat)
+        // let date = parse(datetimeString, this.defaultDateTimeFormat, new Date())
+        return date
       }
-    },
-    dateSelected() {
-      return !this.date
+
+      return null
     }
   },
   methods: {
-    init() {
-      if (!this.datetime) {
-        return
-      }
-
-      let initDateTime
-      if (this.datetime instanceof Date) {
-        initDateTime = this.datetime
-      } else if (typeof this.datetime === 'string' || this.datetime instanceof String) {
-        // see https://stackoverflow.com/a/9436948
-        initDateTime = parse(this.datetime, this.dateTimeFormat, new Date())
-      }
-
-      this.date = format(initDateTime, DEFAULT_DATE_FORMAT)
-      this.time = format(initDateTime, DEFAULT_TIME_FORMAT)
-    },
-    okHandler() {
+    cancelHandler() {
       this.resetPicker()
-      this.$emit('input', this.selectedDatetime)
     },
     clearHandler() {
       this.resetPicker()
@@ -181,19 +182,52 @@ export default {
       this.time = DEFAULT_TIME
       this.$emit('input', null)
     },
+    formatDateTime(selectedDatetime) {
+      return selectedDatetime ? selectedDatetime.format(this.dateTimeFormat) : ''
+      //return selectedDatetime ? format(selectedDatetime, this.dateTimeFormat) : ''
+    },
+    init() {
+      if (!this.value)
+        return
+
+      let initDateTime
+      if (this.datetime instanceof Date)
+        initDateTime = moment(this.value)
+      else if (typeof this.value === 'number' || this.value instanceof Number)
+        initDateTime = moment(this.value)
+      else if (typeof this.value === 'string' || this.value instanceof String) {
+        initDateTime = moment(this.value, this.dateTimeFormat)
+        // see https://stackoverflow.com/a/9436948
+        // initDateTime = parse(this.datetime, this.dateTimeFormat, new Date())
+      }
+
+      this.date = initDateTime.format(DEFAULT_DATE_FORMAT)
+      this.time = initDateTime.format(DEFAULT_TIME_FORMAT)
+      // this.date = format(initDateTime, DEFAULT_DATE_FORMAT)
+      // this.time = format(initDateTime, DEFAULT_TIME_FORMAT)
+    },
+    okHandler() {
+      this.$emit('input', this.internalOutputType == OUTPUT_TYPE_DATE ? this.formatDateTime(this.selectedDatetime) : this.timestamp())
+      this.resetPicker()
+    },
     resetPicker() {
       this.display = false
       this.activeTab = 0
-      if (this.$refs.timer) {
+      if (this.$refs.timer)
         this.$refs.timer.selectingHour = true
-      }
+    },
+    timestamp() {
+      return this.selectedDatetime ? moment(this.selectedDatetime).valueOf() : 0
     },
     showTimePicker() {
       this.activeTab = 1
     }
   },
   watch: {
-    datetime: function() {
+    outputType(newVal) {
+      this.internalOutputType = newVal
+    },
+    value() {
       this.init()
     }
   }
